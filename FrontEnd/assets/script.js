754f91ef46b04b9f63d80c6e3055f8dee7b5d130
Elodie Mitaut <mitautelodie@gmail.com>
2023-10-30 22:37:13 +0100

    affichage des works ds la galerie récup depuis API

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,10 +1,40 @@
-let worksData = []; 
 const fetchWorks = async () => {
-    await fetch("http://localhost:5678/api/works")
-    .then(response => response.json())
-    .then(worksContent => worksData = worksContent)
+  let worksData = [];
+  await fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((worksContent) => (worksData = worksContent));
 
-    console.log(worksData);
-}
+  return worksData;
+};
 
-fetchWorks();
+const displayWorks = async () => {
+  let works = await fetchWorks();
+  let gallery = document.querySelector(".gallery");
+// methode 1
+
+  works.map((work) =>
+    gallery.insertAdjacentHTML(
+      "beforeend",
+      `
+        <figure>
+        <img src=${work.imageUrl} alt="${work.title}">
+        <figcaption>${work.title}</figcaption>
+        </figure>
+        `
+    )
+  );
+
+  // methode 2
+  
+  // gallery.innerHTML = works.map(
+  //   (work) => `
+  //       <figure>
+  //       <img src=${work.imageUrl} alt="${work.title}">
+  //       <figcaption>${work.title}</figcaption>
+  //       </figure>
+  //       `
+  // )
+  //   .join("");// permet de concaténer ce que retourne works.map
+};
+
+displayWorks();
